c7daa60258926fd0dcca1f993faf5025b0848c32
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-05-18 20:58:44 +0200

    clear structure for new project and added app-info component in new test project

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,11 @@
+import './app-info.css'
+
+const AppInfo = () => {
+    return (<div className='app-info'>
+        <h1>Accounting of employees in AnyCompany</h1>
+        <h2>Total number of employees: </h2>
+        <h2>Number of employees will get the prize: </h2>
+    </div>);
+}
+
+export default AppInfo;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-// import {Header} from "./App";
+import App from "./components/app/app";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+    <StrictMode>
+        <App/>
+    </StrictMode>
 );
